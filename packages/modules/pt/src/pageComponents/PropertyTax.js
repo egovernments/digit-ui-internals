68313b1a69b1b2c4c2cd10d903a076458adbd3ec
vamshikrishnakole-wtt-egov <vamshikrishna.kole@egovernments.org>
2021-04-27 22:21:38 +0530

    RAIN-2416 PT Citizen :: The back button should be static on the screen

--- packages/modules/pt/src/pageComponents/PropertyTax.js
+++ packages/modules/pt/src/pageComponents/PropertyTax.js
@@ -1,5 +1,6 @@
 import { Card, CardHeader, CardSubHeader, CardText, SubmitBar } from "@egovernments/digit-ui-react-components";
 import React from "react";
+import { cardBodyStyle } from "../utils";
 //import { map } from "lodash-es";
 
 const PropertyTax = ({ t, config, onSelect, userType, formData }) => {
@@ -18,7 +19,7 @@ const PropertyTax = ({ t, config, onSelect, userType, formData }) => {
     <React.Fragment>
       <Card>
         <CardHeader>{t("PT_DOC_REQ_SCREEN_HEADER")}</CardHeader>
-        <div>
+        <div style={cardBodyStyle}>
           <CardText>{t("PT_DOC_REQ_SCREEN_SUB_HEADER")}</CardText>
           <CardText>{t("PT_DOC_REQ_SCREEN_TEXT")}</CardText>
           <CardText>{t("PT_DOC_REQ_SCREEN_SUB_TEXT")}</CardText>
