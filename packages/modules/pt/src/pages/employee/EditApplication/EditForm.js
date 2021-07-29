2cde5ecb4f8533853f37883426e03a9fb3984de7
harit-egov <harit.saxena@egovernments.org>
2021-07-29 15:04:07 +0530

    corrected update

--- packages/modules/pt/src/pages/employee/EditApplication/EditForm.js
+++ packages/modules/pt/src/pages/employee/EditApplication/EditForm.js
@@ -53,11 +53,8 @@ const EditForm = ({ applicationData }) => {
         let newDoc = data?.documents?.documents?.find((e) => e.documentType.includes(dt[0] + "." + dt[1]));
         return { ...old, ...newDoc };
       }),
-      units: applicationData?.units?.map((old, ind) => {
-        let _new = data?.units?.[ind];
-        if (!_new) return { ...old, active: false };
-        return { ...old, ..._new };
-      }),
+      units: [...applicationData?.units?.map((old) => ({ ...old, active: false })), ...data.units],
+      // units: [...data.units],
       workflow: state.workflow,
       applicationStatus: "UPDATE",
     };
