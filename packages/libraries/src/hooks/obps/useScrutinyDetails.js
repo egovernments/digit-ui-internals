70f02061c72486faa5208722980bc707efc4918d
ayan-egov <ayan.majumder@egovernments.org>
2021-08-09 08:45:41 +0530

    Add DocsRequired and BasicDetails page

--- packages/libraries/src/hooks/obps/useScrutinyDetails.js
+++ packages/libraries/src/hooks/obps/useScrutinyDetails.js
@@ -2,7 +2,7 @@ import { useQuery } from "react-query";
 import { Search } from "../../services/molecules/OBPS/Search";
 
 const useScrutinyDetails = (tenantId, filters, config) => {
-  return useQuery(["OBPS_SCRUTINYDETAILS", filters], () => Search.scrutinyDetails(tenantId, filters));
+  return useQuery(["OBPS_SCRUTINYDETAILS", filters], () => Search.scrutinyDetails(tenantId, filters), config);
 }
 
 export default useScrutinyDetails;
